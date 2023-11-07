4af6070e9b44778f219c71311ee34352fe76c31c
Aubaid <aubaidgull@decisionmanagementsolutions.com>
2023-11-07 15:47:07 +0530

    ProbationTestFiles

---
+++ DMS/src/main/java/com/DMS/Item.java
@@ -0,0 +1,44 @@
+package com.DMS;
+
+public class Item {
+	
+	private int id; 
+	private String name;
+	private String description;
+	private int categoryId;
+	
+	public Item(int id, String name, String description, int categoryId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.categoryId = categoryId;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	
+}
